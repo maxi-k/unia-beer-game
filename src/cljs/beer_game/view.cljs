51c08e0773e76741091566d0b14fac3457987501
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-10-05 18:16:38 +0200

    Rename 'views' to 'view' on client side

--- src/cljs/beer_game/view.cljs
+++ src/cljs/beer_game/view.cljs
@@ -1,24 +1,35 @@
-(ns beer-game.views
+(ns beer-game.view
   (:require [re-frame.core :as re-frame]
             [reagent.core :as reagent]
             [beer-game.client-util :as util]
             [soda-ash.core :as sa]
             [beer-game.components.sidebar :refer [app-menu sidebar-width]]
+            [beer-game.config :as config]
             [beer-game.views.overview :as overview]
             [beer-game.views.statistics :as statistics]
             [beer-game.views.login :as login]))
 
-(def panels
-  {:overview-panel {:title "Übersicht"
-                    :path "#overview"
-                    :icon "dashboard"
-                    :comp overview/overview-panel}
-   :statistics-panel {:title "Statistiken"
-                      :path "#statistics"
-                      :icon "line graph"
-                      :comp statistics/statistics-panel}})
+(defn user->panels
+  [{:as user-data :keys [:user/realm]}]
+  (let [default-panels
+        {:overview-panel {:title "Übersicht"
+                          :path "#overview"
+                          :icon "dashboard"
+                          :comp overview/overview-panel}
+         :statistics-panel {:title "Statistiken"
+                            :path "#statistics"
+                            :icon "line graph"
+                            :comp statistics/statistics-panel}}
+        leader-panels
+        {:events-panel {:title "Events"
+                        :path "#events"
+                        :icon "cubes"}}]
+    (println user-data)
+    (if (= realm config/leader-realm)
+      (merge default-panels leader-panels)
+      default-panels)))
 
-(defn show-panel [panel-name]
+(defn show-panel [panels panel-name]
   [(-> panels
        (get panel-name {:comp :div})
        :comp)])
@@ -60,15 +71,16 @@
         connection (re-frame/subscribe [:client/connected])
         user-data (re-frame/subscribe [:user])]
     (fn []
-      [app-wrapper
-       (if (:auth @user-data)
-         [sa/SidebarPushable
-          [app-menu panels @active-panel]
-          [sa/SidebarPusher {:style {:width (str (- (:width @window-size) sidebar-width)
-                                                 "px")
-                                     :min-width "500px"}}
-           [:main#main-content-wrapper
-            [show-panel @active-panel]]]]
-         [login/login-view])
-       [:div.system-message-tray
-        [connection-state @connection]]])))
+      (let [panels (user->panels user-data)]
+        [app-wrapper
+         (if (:auth @user-data)
+           [sa/SidebarPushable
+            [app-menu panels @active-panel]
+            [sa/SidebarPusher {:style {:width (str (- (:width @window-size) sidebar-width)
+                                                   "px")
+                                       :min-width "500px"}}
+             [:main#main-content-wrapper
+              [show-panel panels @active-panel]]]]
+           [login/login-view])
+         [:div.system-message-tray
+          [connection-state @connection]]]))))
