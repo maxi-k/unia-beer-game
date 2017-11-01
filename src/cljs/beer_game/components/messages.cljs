72dbeba3d13db606cab7d7a9e089e0773bebe2b5
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-11-01 15:24:56 +0100

    Add invalid-game-message and function for rendering message definitions

--- src/cljs/beer_game/components/messages.cljs
+++ src/cljs/beer_game/components/messages.cljs
@@ -4,30 +4,40 @@
             [beer-game.config :as config]
             [beer-game.util :as util]))
 
+(defn render-message
+  [{:as msg
+    :keys [:message/title :message/content :message/icon]}]
+  [sa/Message {:header title
+               :content (if (vector? content)
+                          (ra/as-element content)
+                          (str content))
+               :icon icon}])
+
 (defn debug-msg
   "A message for debugging purposes only."
   [& message]
-  {:message/icon "code"
-   :message/title "Debugging"
-   :message/content [:p (for [msg message] [:p {:key (str msg)} msg
-                                            [:br]])]})
+  #:message{:icon "code"
+            :title "Debugging"
+            :content [:p (for [msg message]
+                           [:p {:key (str msg)} msg
+                            [:br]])]})
 
 (defn no-login-system-msg
   [cause]
-  {:message/icon "exclamation triangle"
-   :message/title "Du wurdest abgemeldet."
-   :message/content [:p "Grund dafür kann ein Neustart des Servers oder Datenverlust des Clients sein."
-                     [:br] "Technische Informationen:"
-                     [:br] cause]})
+  #:message{:icon "exclamation triangle"
+            :title "Du wurdest abgemeldet."
+            :content [:p "Grund dafür kann ein Neustart des Servers oder Datenverlust des Clients sein."
+                      [:br] "Technische Informationen:"
+                      [:br] cause]})
 
 (defn no-permission-system-msg
   [content]
-  {:message/icon "exclamation triangle"
-   :message/title "Keine Berechtigung."
-   :message/content [:p "Aktion konnte aufgrund mangelnder Berechtigungen nicht ausgeführt werden."
-                     [:br]
-                     content]
-   :message/time 5000})
+  #:message{:icon "exclamation triangle"
+            :title "Keine Berechtigung."
+            :content [:p "Aktion konnte aufgrund mangelnder Berechtigungen nicht ausgeführt werden."
+                      [:br]
+                      content]
+            :time 5000})
 
 (defn no-permission-msg
   "A message telling the user that he does not have permission for something."
@@ -41,20 +51,20 @@
 
 (defn logout-forced-msg
   [data]
-  {:message/icon "question circle"
-   :message/title "Du wurdest vom Server ausgeloggt."
-   :message/content [:p "Grund dafür ist wahrscheinlich, dass der Spielleiter das Event beendet hat." [:br]
-                     (if config/development? data)]
-   :message/time 10000})
+  #:message{:icon "question circle"
+            :title "Du wurdest vom Server ausgeloggt."
+            :content [:p "Grund dafür ist wahrscheinlich, dass der Spielleiter das Event beendet hat." [:br]
+                      (if config/development? data)]
+            :time 10000})
 
 (defn event-not-destroyed-msg
   "A message for 'event could not be deleted' response from server."
   [reason]
-  {:message/icon "exclamation triangle"
-   :message/title "Event konnte nicht gelöscht werden."
-   :message/content [:p "Das gewählte Event konnte nicht gelöscht werden. Technische Informationen:"
-                     [:br]
-                     reason]})
+  #:message{:icon "exclamation triangle"
+            :title "Event konnte nicht gelöscht werden."
+            :content [:p "Das gewählte Event konnte nicht gelöscht werden. Technische Informationen:"
+                      [:br]
+                      reason]})
 
 (defn invalid-submission-msg
   "A message for invalid form submissions."
@@ -62,3 +72,11 @@
   #:message {:icon "exclamation triangle"
              :title "Bitte eine vollständige und gültige Eingabe tätigen."
              :time 3000})
+
+(defn invalid-game-data-msg
+  "A message for invalid game data."
+  []
+  #:message {:icon "exclamation triangle"
+             :title "Fehlerhafte Spieldaten."
+             :content [:p
+                       "Die vom Server empfangenen Spieldaten sind fehlerhaft oder unvollständig."]})
