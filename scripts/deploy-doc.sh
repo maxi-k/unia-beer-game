a095c6afb3dd6ada836cfdc0e5466e1757f95deb
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2018-04-14 13:30:11 +0200

    Remove doc deployment script from git, as it is local and irrelevant

--- scripts/deploy-doc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# If parameter was given, package app into jar first
-if [ -n "$1" ]; then
-  boot doc
-fi
-
-# Upload the documents folder
-rsync -r doc/code/ maxi@maximilian-kuschewski.de:/deployments/beer-game/doc
